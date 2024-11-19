eb45998b71344abf7d0b8be0d2ea393d16629bf3
Forest <150213844+Forest48@users.noreply.github.com>
2024-11-18 21:01:30 -0500

    Add files via upload

---
+++ wildshape/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  wildshape
+//
+//  Created by Guest User on 11/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func toList(_ sender: UIButton) {
+        sender.setTitle("[sceneChange]", for: .normal)
+    }
+    
+    
+    @IBAction func toSettings(_ sender: UIButton) {
+        sender.setTitle("unfinished", for: .normal)
+    }
+    
+}
+
